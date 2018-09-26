6ccd80b978588548d71a845ad3c15794865dcd1b
elliot <colinmcrae186@hotmail.com>
2018-09-26 06:13:07 +0000

    Finish Login and Logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,18 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('users.create');
     }
-    public function show(User $user){
-        return view('users.show',compact('user'));
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
-    public function store(Request $request){
-        $this->validate($request,[
-           'name' => 'required|max:50',
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
@@ -26,7 +32,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        return redirect()->route('users.show', [$user]);
     }
 }
